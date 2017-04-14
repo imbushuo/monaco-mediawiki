2636efee5f40c9554ea9aa8772c66a3121d31c42
Bingxing Wang <i@imbushuo.net>
2017-04-14 01:11:40 -0700

    Add experimental argument support.

--- MediaWikiIntelliSense.ts
+++ MediaWikiIntelliSense.ts
@@ -45,16 +45,19 @@ module MwMonacoExtension {
 
     export class TemplateAutoCompletionSource implements ICompletionSource {
 
-        private m_matchRule: RegExp = /{{([^|\n]*)$/;
+        private m_matchRule: RegExp = /{{(.*)$/;
+        private m_templateCache = {};
+        private static g_TemplateQualifier = "|";
+        private static g_TemplateDataRepo = "https://imbushuos3.azureedge.net/Snippets/Moegirlpedia/TemplateData/{0}.json";
 
         get matchRule(): RegExp {
             return this.m_matchRule;
         }
 
-        getCandidateItemsAsync(input: string): monaco.Promise<monaco.languages.CompletionItem[]> {
+        private getTemplateCandidateAsync(templateName: string): monaco.Promise<monaco.languages.CompletionItem[]> {
             // Check input. If starts with ":", query main namespace.
             // Otherwise, query template namespace.
-            const queryNamespace = (input.indexOf(':') === 0) ? 0 : 10;
+            const queryNamespace = (templateName.indexOf(':') === 0) ? 0 : 10;
             return new monaco.Promise<monaco.languages.CompletionItem[]>((complete, error, progress) => {
                 // Input should be santized.
                 mw.loader.using("mediawiki.api").then(() => {
@@ -65,7 +68,7 @@ module MwMonacoExtension {
                         list: "allpages",
                         utf8: 1,
                         formatversion: 2,
-                        apprefix: input,
+                        apprefix: templateName,
                         apnamespace: queryNamespace,
                         aplimit: 20
                     }).then(response => {
@@ -90,6 +93,78 @@ module MwMonacoExtension {
                     });
                 });
             });
+        }
+
+        private getKeys(obj: any): string[] {
+
+            const ret: string[] = [];
+            for (var key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    ret.push(key);
+                }
+            }
+
+            return ret;
+        }
+
+        /**
+         * Get applicable template arguments.
+         * @param input Template raw input.
+         */
+        private getTemplateParametersAsync(input: string) : monaco.Promise<monaco.languages.CompletionItem[]> {
+            return new monaco.Promise<monaco.languages.CompletionItem[]>((complete, error, progress) => {
+                // Template name
+                const templateName = input.substring(0, input.indexOf(TemplateAutoCompletionSource.g_TemplateQualifier));
+                // Check current param and input
+                const currParamInput = input.substring(input.lastIndexOf(TemplateAutoCompletionSource.g_TemplateQualifier) + 1);
+                const paramOpIdx = currParamInput ? currParamInput.indexOf("=") : -1;
+                // Check if argument autocomplete is applicable
+                if (paramOpIdx >= 0) {
+                    complete([]);
+                } else {
+                    if (this.m_templateCache[templateName]) {
+                        complete(this.m_templateCache[templateName]);
+                    } else {
+                        // Query repository for details
+                        $.ajax({
+                            url: TemplateAutoCompletionSource.g_TemplateDataRepo.replace("{0}", encodeURI(templateName)),
+                            cache: true,
+                            dataType: 'json'
+                        }).then(data => {
+                            // Convert template info to autocompletion items
+                            if (data && data.params) {
+                                const keys = this.getKeys(data.params);
+                                const fields: monaco.languages.CompletionItem[] = [];
+                                keys.forEach(key => {
+                                    const d = data.params[key];
+                                    if (d) {
+                                        fields.push({
+                                            label: key,
+                                            documentation: d.description,
+                                            kind: monaco.languages.CompletionItemKind.Property,
+                                            detail: d.description,
+                                            insertText: key
+                                        });
+                                    }
+                                });
+                                this.m_templateCache[templateName] = fields;
+                                complete(fields);
+                            } else {
+                                complete([]);
+                            }
+                        }, () => complete([]));
+                    }
+                }
+            });
+        }
+
+        getCandidateItemsAsync(input: string): monaco.Promise<monaco.languages.CompletionItem[]> {
+            // Check input. If parameter qualifier present, query detailed information.
+            if (input && input.indexOf(TemplateAutoCompletionSource.g_TemplateQualifier) != -1) {
+                return this.getTemplateParametersAsync(input);
+            } else {
+                return this.getTemplateCandidateAsync(input);
+            }
         }
 
     }
